17334fbb9a62ddff5c802aae8defcb545b880d7a
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-06-06 19:52:04 +0200

    more tests

---
+++ src/utils/hooks/use-local-storage/use-local-storage.spec.ts
@@ -0,0 +1,29 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+
+import { functionImportTest } from '@utils/tests';
+import { useLocalStorage } from './use-local-storage.hook';
+
+describe('useLocalStorage Hook', () => {
+  functionImportTest(useLocalStorage);
+
+  beforeEach(() => {
+    localStorage.clear();
+    jest.clearAllMocks();
+  });
+
+  it('should save and return value saved in localStorage', async () => {
+    const { result, waitFor } = renderHook(() => useLocalStorage('key', 'value'));
+
+    const [state, setState] = result.current;
+
+    expect(state).toEqual('value');
+
+    act(() => {
+      setState('new value');
+    });
+
+    await waitFor(() => {
+      expect(result.current[0]).toEqual('new value');
+    });
+  });
+});
