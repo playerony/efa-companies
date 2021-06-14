13a7d491cc5444d3a36544238c0c5b29e4566106
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-06-14 21:32:52 +0200

    🔧 Configure markdown storybook (#17)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,16 @@
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
     '@storybook/addon-links',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
   ],
 };
