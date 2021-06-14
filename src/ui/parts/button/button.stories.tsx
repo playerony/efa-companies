8eaf774738ba1415e2199b1319ec23d0ee8ed20f
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-06-14 21:21:26 +0200

    Configure markdown storybook

--- src/ui/parts/button/button.stories.tsx
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react';
-
-import { Button } from './button.component';
-
-storiesOf('Design System/ui/Button', module).add('default', () => <Button>Button</Button>);
--- src/ui/parts/label/label.stories.tsx
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react';
-
-import { Label } from './label.component';
-
-storiesOf('Design System/ui/Label', module).add('default', () => <Label strong>Label</Label>);
