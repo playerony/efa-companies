c8ef6d347f4733c8a710fbe27e82b9f576d4db16
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-06-15 21:27:49 +0200

    Move API key to .env

--- src/domain/details/hooks/use-details/use-details.hook.ts
+++ src/domain/details/hooks/use-details/use-details.hook.ts
@@ -1,6 +1,8 @@
 import { useGet } from '@utils';
 
+const apiKey = process.env.REACT_APP_API_KEY || 'demo';
+
 const makeApiUrl = (symbol: string): string =>
-  `/query?function=OVERVIEW&symbol=${symbol}&apikey=demo`;
+  `/query?function=OVERVIEW&symbol=${symbol}&apikey=${apiKey}`;
 
 export const useDetails = <T>(symbol: string) => useGet<T>(makeApiUrl(symbol));
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_API_KEY: string;
+    REACT_APP_BASE_API_URL: string;
+  }
+}
