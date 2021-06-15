e3df40f6617248e2ab495bab7b290381b5a20f93
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-06-15 21:00:41 +0200

    ✨ Define base stories (#18)

--- src/ui/parts/empty/empty.stories.tsx
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react';
-
-import { Empty } from './empty.component';
-
-storiesOf('Design System/ui/Empty', module).add('default', () => <Empty />);
--- src/ui/parts/search-input/search-input.stories.tsx
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/react';
-
-import { SearchInput } from './search-input.component';
-
-storiesOf('Design System/ui/SearchInput', module).add('default', () => (
-  <SearchInput placeholder="Search input..." />
-));
